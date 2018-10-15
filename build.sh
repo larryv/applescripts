9242654cc3ee2fb749d639cd2b34c33e06dd4373
Lawrence Velázquez <vq@larryv.me>
2018-10-15 01:28:07 -0400

    Add a rudimentary build script
    Got tired of typing out a different osascript(1) invocation for each JXA
    script.

---
+++ build.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# Compile the AppleScript or JavaScript for Automation source files
+# provided as arguments. Place each compiled script in the same
+# directory as its source.
+
+if [ "$#" -lt 1 ]; then
+    printf 'Usage: %s script1 [script2 ...]\n' "$0" >&2
+    exit 1
+fi
+
+for infile; do
+    if [ "${infile##*.}" = js ]; then
+        OSALANG='-l JavaScript'
+    fi
+    outfile=${infile%.*}.scpt
+    osacompile $OSALANG -o "$outfile" "$infile" || exit
+
+    # osascript(1) prints the result to stdout, which is pointless here.
+    osascript - "$outfile" >/dev/null <<'EOF'
+on run argv
+    tell application "Finder"
+        set extension hidden of (POSIX file (item 1 of argv) as alias) to true
+    end tell
+end run
+EOF
+done
