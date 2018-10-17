0f5c81922e35793135401b7112fb1413a6078da1
Lawrence Velázquez <vq@larryv.me>
2018-10-17 00:15:22 -0400

    EagleFiler "Rename Chats": Handle "continuations"
    I had previously assumed that all iChat/Messages transcripts were named
    "[Person] on [YYYY-mm-dd] at [HH].[MM].ichat". This turned out to be
    incorrect. Consider this sequence of events:
    
        1. On 2018-10-17, the system clock transitions from 00:40 to 00:41.
        2. I begin a new conversation with John Doe.
        3. I terminate that conversation by closing the chat window.
        4. I begin a new conversation with John Doe.
        5. The system clock transitions from 00:41 to 00:42.
    
    The "2018-10-17" archive folder could not contain two files named "John
    Doe on 2018-10-17 at 00.41.ichat", so iChat/Messages would instead save
    the second one as "John Doe on 2018-10-17 at 00.41 #2.ichat". Presumably
    this would (with increasing effort) extend to #3, #4, #5, etc.
    
    Adapt the script to handle these "continuations". The numbers are now
    incorporated into the titles and basenames of such records.

--- EagleFiler/Rename Chats to "[YYYYmmdd]T[HHMM] [Person]".js
+++ EagleFiler/Rename Chats to "[YYYYmmdd]T[HHMM] [Person]".js
@@ -2,31 +2,39 @@
  * Rename Chats to "[YYYYmmdd]T[HHMM] [Person]"
  *
  * If any selected EagleFiler records are iChat/Messages transcripts
- * with basenames in the default form "[Person] on [YYYY]-[mm]-[dd] at
- * [HH].[MM]", set their fields as follows:
+ * with basenames in the default forms "[Person] on [YYYY]-[mm]-[dd] at
+ * [HH].[MM]" or "[Person] on [YYYY]-[mm]-[dd] at [HH].[MM] #[num]", set
+ * their fields as follows:
  *
  *      title:      [YYYY]-[mm]-[dd] [HH]:[MM]
+ *                  or
+ *                  [YYYY]-[mm]-[dd] [HH]:[MM] (#[num])
+ *
  *      from name:  [Person]
+ *
  *      basename:   [YYYY][mm][dd]T[HH][MM] [Person]
+ *                  or
+ *                  [YYYY][mm][dd]T[HH][MM] [Person] (#[num])
  *
  * Selected records that are not transcripts or that do not have default
  * basenames are not modified.
  *
- * Last updated 2018-10-16.
+ * Last updated 2018-10-17.
  */
 
 'use strict';
 
 // Ideally this would be const, but global const/let declarations
 // interact poorly with OSA persistence.
-var regex = /^(.+?) on (\d{4})-(\d{2})-(\d{2}) at (\d{2})\.(\d{2})$/;
+var regex = /^(.+?) on (\d{4})-(\d{2})-(\d{2}) at (\d{2})\.(\d{2})(?: (#\d+))?$/;
 
 Application('com.c-command.EagleFiler').browserWindows[0].selectedRecords()
     .filter(record => record.kind() === 'Chat')
     .map(record => ({record, metadata: regex.exec(record.basename())}))
     .filter(({metadata}) => metadata)
-    .forEach(({record, metadata: [, person, YYYY, mm, dd, HH, MM]}) => {
-        record.title = `${YYYY}-${mm}-${dd} ${HH}:${MM}`;
+    .forEach(({record, metadata: [, person, YYYY, mm, dd, HH, MM, num]}) => {
+        const suffix = num ? ` (${num})` : '';
+        record.title = `${YYYY}-${mm}-${dd} ${HH}:${MM}${suffix}`;
         record.fromName = person;
-        record.basename = `${YYYY}${mm}${dd}T${HH}${MM} ${person}`;
+        record.basename = `${YYYY}${mm}${dd}T${HH}${MM} ${person}${suffix}`;
     });
