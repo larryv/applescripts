3dbccfdba03bcad304abd8cb02a13be188b712f6
Lawrence Velázquez <vq@larryv.me>
2018-10-24 20:47:40 -0400

    EagleFiler "Rename Chats": Rewrite completely
    - Extract the "start time" from chat transcripts' contents instead of
      parsing it out of their filenames. This mitigates an issue wherein,
      due to various bugs, filename times can differ significantly from
      internal times. (I came across one where the filename time preceded
      the message times by several MONTHS.) The internal times are still
      often inaccurate, but much less so than the filename times.
    
    - Similarly, extract participants' names from transcript contents
      instead of parsing them out of filenames; the contents often contain
      better information. (For instance, a transcript contains a list of all
      participants of a group message, but its filename only says "Chat with
      [PARTICIPANT] et al".)
    
    - In titles and filenames, use UTC time instead of local time. This
      ensures that records' times are directly comparable in perpetuity,
      even if I move to another time zone.
    
    - Process all chats, regardless of filename format. Abandonment of
      filename parsing means that filenames no longer have to adhere to
      a particular format.
    
    - Log all skipped records, to aid in debugging.
    
    - Rename back to "Process Chats" because it does more than just rename
      records.

---
+++ EagleFiler/Process Chats.js
@@ -0,0 +1,120 @@
+/*
+ * Process Chats
+ *
+ * If any selected EagleFiler records are iChat/Messages transcripts,
+ * extract their starting times and participants' names (excluding
+ * mine!) and set their EagleFiler fields as follows:
+ *
+ *      title:      [YYYY]-[MM]-[DD] [hh]:[mm]:[ss] UTC
+ *      from name:  [Participants]
+ *      basename:   [YYYY][MM][DD]T[hh][mm][ss]Z [Participants]
+ *
+ * (Note that transcripts often contain inaccurate timestamps, due to
+ * bugs I don't understand. Start times are best understood as
+ * approximations.)
+ *
+ * Selected records that are not transcripts are not modified.
+ *
+ * Last updated 2018-11-02.
+ */
+
+'use strict';
+
+/*
+ * Given a Date, return an object containing string properties for its
+ * year, month (01-12), day, hour (00-23), minute, and second. All
+ * properties other than the year are zero-padded to length 2.
+ */
+function getFormattedDateComponentsInUTC(date) {
+    return {
+        YYYY: date.getUTCFullYear().toString(),
+        // getUTCMonth() is zero-indexed (January = 0, etc.).
+        MM: (date.getUTCMonth() + 1).toString().padStart(2, '0'),
+        DD: date.getUTCDate().toString().padStart(2, '0'),
+        hh: date.getUTCHours().toString().padStart(2, '0'),
+        mm: date.getUTCMinutes().toString().padStart(2, '0'),
+        ss: date.getUTCSeconds().toString().padStart(2, '0'),
+    };
+}
+
+/*
+ * Given a Path to an iChat/Messages transcript, return an object
+ * containing the chat's starting time and participants' names. (Note
+ * that transcripts often contain inaccurate timestamps, due to bugs
+ * I don't understand. Start times are best understood as
+ * approximations.)
+ */
+function metadataFromChat(path) {
+    const unarchiver = openNSKeyedUnarchiver(path.toString());
+    const metadata = unarchiver.decodeObjectForKey('metadata');
+    if (metadata.isNil()) {
+        throw new Error(`Could not extract metadata from “${path}”.`);
+    }
+    // Probably unnecessary in this context, but there's something to be
+    // said for doing things properly.
+    unarchiver.finishDecoding;
+    return {
+        participants: ObjC.deepUnwrap(metadata.objectForKey('Participants')),
+        startTime: ObjC.unwrap(metadata.objectForKey('StartTime')),
+    };
+}
+
+/*
+ * Given a string representing the path to a Cocoa keyed archive, return
+ * an NSKeyedUnarchiver on the archive's data.
+ */
+function openNSKeyedUnarchiver(path) {
+    const error = $();
+    const data =
+            $.NSData.alloc.initWithContentsOfFileOptionsError(path, 0, error);
+    if (data.isNil()) {
+        throw new Error(ObjC.unwrap(error.localizedDescription));
+    }
+    const unarchiver =
+            $.NSKeyedUnarchiver.alloc.initForReadingFromDataError(data, error);
+    if (unarchiver.isNil()) {
+        throw new Error(ObjC.unwrap(error.localizedDescription));
+    }
+
+    // Can't seem to decode anything without turning this off.
+    unarchiver.requiresSecureCoding = false;
+    // Exceptions contain useful information.
+    unarchiver.decodingFailurePolicy = $.NSDecodingFailurePolicyRaiseException;
+
+    return unarchiver;
+}
+
+function run() {
+    const EagleFiler = Application('com.c-command.EagleFiler');
+    const records = EagleFiler.browserWindows[0].selectedRecords();
+    Progress.totalUnitCount = records.length;
+
+    for (const [idx, record] of records.entries()) {
+        Progress.completedUnitCount = idx;
+
+        const recordFile = record.file();
+        let metadata;
+        try {
+            if (record.kind() !== 'Chat') {
+                throw new Error('Not a chat, according to EagleFiler');
+            }
+            metadata = metadataFromChat(recordFile);
+        } catch (error) {
+            $.NSLog('%@: Skipping “%@” (%@)', $('Process Chats'),
+                    $(recordFile.toString()), $(error.toString()));
+            continue;
+        }
+
+        const {YYYY, MM, DD, hh, mm, ss} =
+                getFormattedDateComponentsInUTC(metadata.startTime);
+        const participants = metadata.participants
+                .filter(name => !/^Lawrence (?:.+ )?Vel[aá]zquez$/u.test(name))
+                .sort((a, b) => a.localeCompare(b))
+                .join(', ');
+
+        record.title = `${YYYY}-${MM}-${DD} ${hh}:${mm}:${ss} UTC`;
+        record.fromName = participants;
+        record.basename = `${YYYY}${MM}${DD}T${hh}${mm}${ss}Z ${participants}`;
+    }
+    Progress.completedUnitCount++;
+}
